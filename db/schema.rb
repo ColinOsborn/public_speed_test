a594d332010664132ce5b57d1a16e0c838a72756
Colin Osborn <colin.osborn@gmail.com>
2024-11-20 11:54:43 -0700

    implemented internet speeds table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_20_183511) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_20_184732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "internet_speeds", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "download_speed", null: false
+    t.string "download_units", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "place_id", null: false
+    t.index ["place_id"], name: "index_internet_speeds_on_place_id"
+  end
 
   create_table "places", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_20_183511) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "internet_speeds", "places"
 end
