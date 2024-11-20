b40a2b1a0468cd5c11d27f856ab9a012d58c01db
Colin Osborn <colin.osborn@gmail.com>
2024-11-20 11:46:21 -0700

    implemented places model and sample places in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Place.create!(
+  name: 'Starbucks',
+  address: '719 E Valley Rd',
+  city: 'Basalt, CO'
+)
+Place.create!(
+  name: 'Dos Gringos',
+  address: '588 Highway 133',
+  city: 'Carbondale, CO'
+)
