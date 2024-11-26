fe88abbd054974f6991ee0e5d4a9303bba998c6e
Colin Osborn <colin.osborn@gmail.com>
2024-11-25 22:34:56 -0700

    controller logic update

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -2,7 +2,26 @@ module API
   class PlacesController
 
     def index
-      @places = Place.all
+      places = Place.all.map do |place|
+        {
+          name: place.name,
+          city: place.city,
+
+        }
+      end
+    end
+
+    private
+
+    # These are just here temporarily until
+    # I find the correct class to store them
+    # it presenter/decorartor etc
+    def avg_download_speed(place)
+      (place % 2)
+    end
+
+    def download_speed_units
+
     end
   end
 end
